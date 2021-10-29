266a46698f7875873ebefe25bc01516044513ad2
qqpayne <ivanakostelov@gmail.com>
2021-10-30 00:37:26 +0300

    Main logic

---
+++ bot.py
@@ -0,0 +1,45 @@
+from telebot import TeleBot
+from random import randint
+
+from os import environ
+
+
+bot = TeleBot(environ["BOT_TOKEN"])
+
+HELP_TEXT = """
+Flip a coin. The output will be heads or tails.
+
+Use following commands:
+/start - start bot
+/help - help text
+/coin - flip a coin
+"""
+
+START_TEXT = """
+A simple bot which allows solve questions of your life by flipping a coin. 
+"""
+
+
+@bot.message_handler(commands=["help"])
+def help_message(message):
+    bot.send_message(message.chat.id, HELP_TEXT)
+
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    bot.send_message(message.chat.id, START_TEXT)
+
+
+@bot.message_handler(commands=["coin"])
+def flip_coin(message):
+    random = randint(0, 1)
+    text = "Heads" if random == 1 else "Tails"
+    bot.send_message(message.chat.id, text)
+
+
+def main():
+    bot.polling()
+
+
+if __name__ == "__main__":
+    main()
