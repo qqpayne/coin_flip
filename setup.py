399f0b5b3b7c54b94b9a3008306750b0dcddf01b
qqpayne <ivanakostelov@gmail.com>
2021-10-30 01:29:00 +0300

    Setup script

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = ["pyTelegramBotAPI"]
+
+setup(
+    name="coin_flip",
+    version="1.0",
+    description="Telegram bot that let's you flip a coin in a dialog",
+    classifiers=["Programming Language :: Python"],
+    author="",
+    author_email="",
+    keywords="",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={},
+    data_files=[("config", ["cfg.ini"])],
+    install_requires=requires,
+    entry_points={
+        "console_scripts": [
+            "start = bot:main",
+        ],
+    },
+)
